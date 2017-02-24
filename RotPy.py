* Fixed the scale parameter r0 in the calcuation of all the potentials * Read other test rotation curve * Output parameters for the potentials used to fit the rotation curve * Printing some Dark Halo parameters (Virial mass and concentration)

RotPy.py
@@ -59,22 +59,24 @@ amp5=input_params['mass'][4]
 h_r=input_params['a (kpc)'][3]
 amp4=input_params['mass'][3]
 
-x_offset = 0.35 # It defines a radial coordinate offset as user input
+x_offset = 0.0 # 0.35 # It defines a radial coordinate offset as user input
+r_0=1*units.kpc
+v_0=220*units.km/units.s
 
 name='rot_curve.txt' # Set the name of the galaxy's txt file rotation curve
 tt=Table.Table.read(name, format='ascii.tab')
-r_califa=tt['r']-x_offset # The txt file must contain the radial coordinate values in kpc
-v_c_califa=tt['vel'] # velocity in km/s
-v_c_err_califa = tt['e_vel'] # and velocity error in km/s
+r_data=tt['r']-x_offset # The txt file must contain the radial coordinate values in kpc
+v_c_data=tt['vel'] # velocity in km/s
+v_c_err_data = tt['e_vel'] # and velocity error in km/s
 
 
-lista=np.linspace(0.00001, np.max(r_califa), 10*len(r_califa)) # radial coordinate for the rotation curve calculation
+lista=np.linspace(0.00001, np.max(r_data), 10*len(r_data)) # radial coordinate for the rotation curve calculation
 # Potentials definition using physical units (amplitude in Solar masses, scales in kpc and surface density in Solar masses / pc^2 )
-MN_Bulge_p= MiyamotoNagaiPotential(amp=amp1*units.Msun,a=a1*units.kpc,b=b1*units.kpc,normalize=False,ro=8*units.kpc, vo=220*units.km/units.s)
-MN_Thin_Disk_p= MiyamotoNagaiPotential(amp=amp2*units.Msun,a=a2*units.kpc,b=b2*units.kpc,normalize=False,ro=8*units.kpc, vo=220*units.km/units.s)
-MN_Thick_Disk_p= MiyamotoNagaiPotential(amp=amp3*units.Msun,a=a3*units.kpc,b=b3*units.kpc,normalize=False,ro=8*units.kpc, vo=220*units.km/units.s)
-EX_Disk_p = RazorThinExponentialDiskPotential(amp=amp4*(units.Msun/(units.pc**2)), hr=h_r*units.kpc, maxiter=20, tol=0.001, normalize=False, ro=8*units.kpc, vo=220*units.km/units.s, new=True, glorder=100)
-NFW_p = NFWPotential(amp=amp5*units.Msun, a=a5*units.kpc, normalize=False)
+MN_Bulge_p= MiyamotoNagaiPotential(amp=amp1*units.Msun,a=a1*units.kpc,b=b1*units.kpc,normalize=False,ro=r_0, vo=v_0)
+MN_Thin_Disk_p= MiyamotoNagaiPotential(amp=amp2*units.Msun,a=a2*units.kpc,b=b2*units.kpc,normalize=False,ro=r_0, vo=v_0)
+MN_Thick_Disk_p= MiyamotoNagaiPotential(amp=amp3*units.Msun,a=a3*units.kpc,b=b3*units.kpc,normalize=False,ro=r_0, vo=v_0)
+EX_Disk_p = RazorThinExponentialDiskPotential(amp=amp4*(units.Msun/(units.pc**2)), hr=h_r*units.kpc, maxiter=20, tol=0.001, normalize=False, ro=r_0, vo=v_0, new=True, glorder=100)
+NFW_p = NFWPotential(amp=amp5*units.Msun, a=a5*units.kpc, normalize=False, ro=r_0, vo=v_0)
 # Circular velocities in km/s
 MN_Bulge = calcRotcurve(MN_Bulge_p, lista, phi=None)*220
 MN_Thin_Disk = calcRotcurve(MN_Thin_Disk_p, lista, phi=None)*220
@@ -87,15 +89,15 @@ v_circ_comp = calcRotcurve([MN_Bulge_p,MN_Thin_Disk_p,MN_Thick_Disk_p, EX_Disk_p
 # Global variables for the plot
 fig = plt.figure()
 ax = fig.add_axes((0.35, 0.1, 0.6, 0.85))
-ax.set_xlim([0, r_califa[-1]])
+ax.set_xlim([0, r_data[-1]])
 # A plot for each rotation curve with the colors indicated below
 MN_b_plot, = ax.plot(lista, MN_Bulge, linestyle='--', c='gray')
 MN_td_plot, = ax.plot(lista, MN_Thin_Disk, linestyle='--', c='red')
 MN_tkd_plot, = ax.plot(lista, MN_Thick_Disk, linestyle='--', c='blue')
 EX_d_plot, = ax.plot(lista, EX_Disk, linestyle='--', c='cyan')
 NFW_plot, = ax.plot(lista, NFW, linestyle='--', c='green')
-CV_galaxy = ax.errorbar(r_califa, v_c_califa, v_c_err_califa,  c='k', fmt='', ls='none')
-CV_galaxy_dot = ax.scatter(r_califa, v_c_califa, c='k')
+CV_galaxy = ax.errorbar(r_data, v_c_data, v_c_err_data,  c='k', fmt='', ls='none')
+CV_galaxy_dot = ax.scatter(r_data, v_c_data, c='k')
 # Composed rotation curve
 v_circ_comp_plot, = ax.plot(lista, v_circ_comp, c='k')
 ax.cla()
@@ -105,12 +107,12 @@ ax.cla()
 ax.set_title(name, fontsize=30)
 ax.set_xlabel(r'$R$ (kpc)', fontsize=20)
 ax.set_ylabel(r'$v_c$ (km/s)', fontsize=20)
-ax.set_xlim([0, r_califa[-1]])
-ax.set_ylim([0,300])
-MN_Bulge_p= MiyamotoNagaiPotential(amp=amp1*units.Msun,a=a1*units.kpc,b=b1*units.kpc,normalize=False,ro=8*units.kpc, vo=220*units.km/units.s)
-MN_Thin_Disk_p= MiyamotoNagaiPotential(amp=amp2*units.Msun,a=a2*units.kpc,b=b2*units.kpc,normalize=False,ro=8*units.kpc, vo=220*units.km/units.s)
-MN_Thick_Disk_p= MiyamotoNagaiPotential(amp=amp3*units.Msun,a=a3*units.kpc,b=b3*units.kpc,normalize=False,ro=8*units.kpc, vo=220*units.km/units.s)
-EX_Disk_p = RazorThinExponentialDiskPotential(amp=amp4*(units.Msun/(units.pc**2)), hr=h_r*units.kpc, maxiter=20, tol=0.001, normalize=False, ro=8*units.kpc, vo=220*units.km/units.s, new=True, glorder=100)
+ax.set_xlim([0, r_data[-1]])
+ax.set_ylim([0,np.max(v_c_data)*1.1])
+MN_Bulge_p= MiyamotoNagaiPotential(amp=amp1*units.Msun,a=a1*units.kpc,b=b1*units.kpc,normalize=False,ro=r_0, vo=v_0)
+MN_Thin_Disk_p= MiyamotoNagaiPotential(amp=amp2*units.Msun,a=a2*units.kpc,b=b2*units.kpc,normalize=False,ro=r_0, vo=v_0)
+MN_Thick_Disk_p= MiyamotoNagaiPotential(amp=amp3*units.Msun,a=a3*units.kpc,b=b3*units.kpc,normalize=False,ro=r_0, vo=v_0)
+EX_Disk_p = RazorThinExponentialDiskPotential(amp=amp4*(units.Msun/(units.pc**2)), hr=h_r*units.kpc, maxiter=20, tol=0.001, normalize=False, ro=r_0, vo=v_0, new=True, glorder=100)
 NFW_p = NFWPotential(amp=amp5*units.Msun, a=a5*units.kpc, normalize=False)
 
 MN_Bulge = calcRotcurve(MN_Bulge_p, lista, phi=None)*220
@@ -125,8 +127,8 @@ MN_td_plot, = ax.plot(lista, MN_Thin_Disk, linestyle='--', c='red')
 MN_tkd_plot, = ax.plot(lista, MN_Thick_Disk, linestyle='--', c='blue')
 EX_d_plot, = ax.plot(lista, EX_Disk, linestyle='--', c='cyan')
 NFW_plot, = ax.plot(lista, NFW, linestyle='--', c='green')
-CV_galaxy = ax.errorbar(r_califa, v_c_califa, v_c_err_califa,  c='k', fmt='', ls='none')
-CV_galaxy_dot = ax.scatter(r_califa, v_c_califa, c='k')
+CV_galaxy = ax.errorbar(r_data, v_c_data, v_c_err_data,  c='k', fmt='', ls='none')
+CV_galaxy_dot = ax.scatter(r_data, v_c_data, c='k')
 v_circ_comp_plot, = ax.plot(lista, v_circ_comp, c='k')
 
 # Checkbox for selecting the potentials to compose the rotation
@@ -171,68 +173,80 @@ def MN_b_amp_s_func(val):
         global MN_Bulge_p, amp1, a1, b1
         amp1=val*1
         #MN_b_amp_s.valtext.set_text(10**val)
-        MN_Bulge_p = MiyamotoNagaiPotential(amp=val*units.Msun,a=a1*units.kpc,b=b1*units.kpc,normalize=False,ro=8*units.kpc, vo=220*units.km/units.s)   
+        MN_Bulge_p = MiyamotoNagaiPotential(amp=val*units.Msun,a=a1*units.kpc,b=b1*units.kpc,normalize=False,ro=r_0, vo=v_0)   
         update_rot_curve()
 def MN_b_a_s_func(val):
-    global MN_Bulge_p, amp1, a1, b1
-    a1=val*1
-    MN_Bulge_p = MiyamotoNagaiPotential(amp=amp1*units.Msun,a=val*units.kpc,b=b1*units.kpc,normalize=False,ro=8*units.kpc, vo=220*units.km/units.s)   
-    update_rot_curve()
+    if MN_b_plot.get_visible() == True:
+        global MN_Bulge_p, amp1, a1, b1
+        a1=val*1
+        MN_Bulge_p = MiyamotoNagaiPotential(amp=amp1*units.Msun,a=val*units.kpc,b=b1*units.kpc,normalize=False,ro=r_0, vo=v_0)   
+        update_rot_curve()
 def MN_b_b_s_func(val):
-    global MN_Bulge_p, amp1, a1, b1
-    b1=val*1
-    MN_Bulge_p = MiyamotoNagaiPotential(amp=amp1*units.Msun,a=a1*units.kpc,b=val*units.kpc,normalize=False,ro=8*units.kpc, vo=220*units.km/units.s)   
-    update_rot_curve()
+    if MN_b_plot.get_visible() == True:
+        global MN_Bulge_p, amp1, a1, b1
+        b1=val*1
+        MN_Bulge_p = MiyamotoNagaiPotential(amp=amp1*units.Msun,a=a1*units.kpc,b=val*units.kpc,normalize=False,ro=r_0, vo=v_0)   
+        update_rot_curve()
 def MN_td_amp_s_func(val):
-    global MN_Thin_Disk_p, amp2, a2, b2
-    amp2=val*1
-    MN_Thin_Disk_p= MiyamotoNagaiPotential(amp=val*units.Msun,a=a2*units.kpc,b=b2*units.kpc,normalize=False,ro=8*units.kpc, vo=220*units.km/units.s)  
-    update_rot_curve()
+    if MN_td_plot.get_visible() == True:
+        global MN_Thin_Disk_p, amp2, a2, b2
+        amp2=val*1
+        MN_Thin_Disk_p= MiyamotoNagaiPotential(amp=val*units.Msun,a=a2*units.kpc,b=b2*units.kpc,normalize=False,ro=r_0, vo=v_0)  
+        update_rot_curve()
 def MN_td_a_s_func(val):
-    global MN_Thin_Disk_p, amp2, a2, b2
-    a2=val*1
-    MN_Thin_Disk_p= MiyamotoNagaiPotential(amp=amp2*units.Msun,a=val*units.kpc,b=b2*units.kpc,normalize=False,ro=8*units.kpc, vo=220*units.km/units.s)  
-    update_rot_curve()
+    if MN_td_plot.get_visible() == True:
+        global MN_Thin_Disk_p, amp2, a2, b2
+        a2=val*1
+        MN_Thin_Disk_p= MiyamotoNagaiPotential(amp=amp2*units.Msun,a=val*units.kpc,b=b2*units.kpc,normalize=False,ro=r_0, vo=v_0)  
+        update_rot_curve()
 def MN_td_b_s_func(val):
-    global MN_Thin_Disk_p, amp2, a2, b2
-    b2=val*1
-    MN_Thin_Disk_p= MiyamotoNagaiPotential(amp=amp2*units.Msun,a=a2*units.kpc,b=val*units.kpc,normalize=False,ro=8*units.kpc, vo=220*units.km/units.s)  
-    update_rot_curve()
+    if MN_td_plot.get_visible() == True:
+        global MN_Thin_Disk_p, amp2, a2, b2
+        b2=val*1
+        MN_Thin_Disk_p= MiyamotoNagaiPotential(amp=amp2*units.Msun,a=a2*units.kpc,b=val*units.kpc,normalize=False,ro=r_0, vo=v_0)  
+        update_rot_curve()
 def MN_tkd_amp_s_func(val):
-    global MN_Thick_Disk_p, amp3, a3, b3
-    amp3=val*1
-    MN_Thick_Disk_p= MiyamotoNagaiPotential(amp=val*units.Msun,a=a3*units.kpc,b=b3*units.kpc,normalize=False,ro=8*units.kpc, vo=220*units.km/units.s)  
-    update_rot_curve()
+    if MN_tkd_plot.get_visible() == True:
+        global MN_Thick_Disk_p, amp3, a3, b3
+        amp3=val*1
+        MN_Thick_Disk_p= MiyamotoNagaiPotential(amp=val*units.Msun,a=a3*units.kpc,b=b3*units.kpc,normalize=False,ro=r_0, vo=v_0)  
+        update_rot_curve()
 def MN_tkd_a_s_func(val):
-    global MN_Thick_Disk_p, amp3, a3, b3
-    a3=val*1
-    MN_Thick_Disk_p= MiyamotoNagaiPotential(amp=amp3*units.Msun,a=val*units.kpc,b=b3*units.kpc,normalize=False,ro=8*units.kpc, vo=220*units.km/units.s)  
-    update_rot_curve()
+    if MN_tkd_plot.get_visible() == True:
+        global MN_Thick_Disk_p, amp3, a3, b3
+        a3=val*1
+        MN_Thick_Disk_p= MiyamotoNagaiPotential(amp=amp3*units.Msun,a=val*units.kpc,b=b3*units.kpc,normalize=False,ro=r_0, vo=v_0)  
+        update_rot_curve()
 def MN_tkd_b_s_func(val):
-    global MN_Thick_Disk_p, amp3, a3, b3
-    b3=val*1
-    MN_Thick_Disk_p= MiyamotoNagaiPotential(amp=amp3*units.Msun,a=a3*units.kpc,b=val*units.kpc,normalize=False,ro=8*units.kpc, vo=220*units.km/units.s)  
-    update_rot_curve()
+    if MN_tkd_plot.get_visible() == True:
+        global MN_Thick_Disk_p, amp3, a3, b3
+        b3=val*1
+        MN_Thick_Disk_p= MiyamotoNagaiPotential(amp=amp3*units.Msun,a=a3*units.kpc,b=val*units.kpc,normalize=False,ro=r_0, vo=v_0)  
+        update_rot_curve()
 def NFW_amp_s_func(val):
-    global NFW_p, amp5,a5
-    amp5=val*1
-    NFW_p = NFWPotential(amp=val*units.Msun, a=a5*units.kpc, normalize=False)
-    update_rot_curve()    
+    if NFW_plot.get_visible() == True:
+        global NFW_p, amp5,a5
+        amp5=val*1
+        NFW_p = NFWPotential(amp=val*units.Msun, a=a5*units.kpc, normalize=False, ro=r_0, vo=v_0)
+        update_rot_curve()    
 def NFW_a_s_func(val):
-    global NFW_p, amp5,a5
-    a5=val*1
-    NFW_p = NFWPotential(amp=amp5*units.Msun, a=val*units.kpc, normalize=False)
-    update_rot_curve()
+    if NFW_plot.get_visible() == True:
+        global NFW_p, amp5,a5
+        a5=val*1
+        NFW_p = NFWPotential(amp=amp5*units.Msun, a=val*units.kpc, normalize=False, ro=r_0, vo=v_0)
+        update_rot_curve()
 def MN_ed_amp_s_func(val):
-    global EX_Disk_p, amp4,h_r
-    amp4=val*1
-    EX_Disk_p = RazorThinExponentialDiskPotential(amp=val*(units.Msun/(units.pc**2)), hr=h_r*units.kpc, maxiter=20, tol=0.001, normalize=False, ro=8*units.kpc, vo=220*units.km/units.s, new=True, glorder=100)
-    update_rot_curve() 
+    if EX_d_plot.get_visible() == True:
+        global EX_Disk_p, amp4,h_r
+        amp4=val*1
+        EX_Disk_p = RazorThinExponentialDiskPotential(amp=val*(units.Msun/(units.pc**2)), hr=h_r*units.kpc, maxiter=20, tol=0.001, normalize=False, ro=r_0, vo=v_0, new=True, glorder=100)
+        update_rot_curve() 
 def MN_ed_a_s_func(val):
-    global EX_Disk_p, amp4,h_r
-    h_r=val*1
-    EX_Disk_p = RazorThinExponentialDiskPotential(amp=amp4*(units.Msun/(units.pc**2)), hr=val*units.kpc, maxiter=20, tol=0.001, normalize=False, ro=8*units.kpc, vo=220*units.km/units.s, new=True, glorder=100)
-    update_rot_curve()
+    if EX_d_plot.get_visible() == True:
+        global EX_Disk_p, amp4,h_r
+        h_r=val*1
+        EX_Disk_p = RazorThinExponentialDiskPotential(amp=amp4*(units.Msun/(units.pc**2)), hr=val*units.kpc, maxiter=20, tol=0.001, normalize=False, ro=r_0, vo=v_0, new=True, glorder=100)
+        update_rot_curve()
     
 # update rotation curve for the selected and the composed potential
 def update_rot_curve():
@@ -241,8 +255,8 @@ def update_rot_curve():
     composite_pot_array=[]
     ax.set_xlabel(r'$R$ (kpc)', fontsize=20)
     ax.set_ylabel(r'$v_c$ (km/s)', fontsize=20)
-    ax.set_xlim([0, r_califa[-1]])
-    ax.set_ylim([0,300])
+    ax.set_xlim([0, r_data[-1]])
+    ax.set_ylim([0,np.max(v_c_data)*1.1])
     if MN_b_plot.get_visible() == True:
         MN_Bulge = calcRotcurve(MN_Bulge_p, lista, phi=None)*220
         MN_b_plot, = ax.plot(lista, MN_Bulge, linestyle='--', c='gray')
@@ -263,8 +277,8 @@ def update_rot_curve():
         EX_Disk = calcRotcurve(EX_Disk_p, lista, phi=None)*220
         EX_d_plot, = ax.plot(lista, EX_Disk, linestyle='--', c='cyan')
         composite_pot_array.append(EX_Disk_p)
-    CV_galaxy = ax.errorbar(r_califa, v_c_califa, v_c_err_califa,  c='k', fmt='', ls='none')
-    CV_galaxy_dot = ax.scatter(r_califa, v_c_califa, c='k')
+    CV_galaxy = ax.errorbar(r_data, v_c_data, v_c_err_data,  c='k', fmt='', ls='none')
+    CV_galaxy_dot = ax.scatter(r_data, v_c_data, c='k')
     v_circ_comp = calcRotcurve(composite_pot_array, lista, phi=None)*220
     v_circ_comp_plot, = ax.plot(lista, v_circ_comp, c='k')
 
@@ -277,15 +291,15 @@ v_circ_comp = calcRotcurve([MN_Bulge_p,MN_Thin_Disk_p,MN_Thick_Disk_p, NFW_p], l
 
 ax.set_xlabel(r'$R$ (kpc)', fontsize=20)
 ax.set_ylabel(r'$v_c$ (km/s)', fontsize=20)
-ax.set_xlim([0, r_califa[-1]])
-ax.set_ylim([0,300])
+ax.set_xlim([0, r_data[-1]])
+ax.set_ylim([0,np.max(v_c_data)*1.1])
 MN_b_plot, = ax.plot(lista, MN_Bulge, linestyle='--', c='gray')
 MN_td_plot, = ax.plot(lista, MN_Thin_Disk, linestyle='--', c='red')
 MN_tkd_plot, = ax.plot(lista, MN_Thick_Disk, linestyle='--', c='blue')
 EX_d_plot, = ax.plot(lista, EX_Disk, linestyle='--', c='cyan')
 NFW_plot, = ax.plot(lista, NFW, linestyle='--', c='green')
-CV_galaxy = ax.errorbar(r_califa, v_c_califa, v_c_err_califa,  c='k', fmt='', ls='none')
-CV_galaxy_dot = ax.scatter(r_califa, v_c_califa, c='k')
+CV_galaxy = ax.errorbar(r_data, v_c_data, v_c_err_data,  c='k', fmt='', ls='none')
+CV_galaxy_dot = ax.scatter(r_data, v_c_data, c='k')
 v_circ_comp_plot, = ax.plot(lista, v_circ_comp, c='k')
 
 
@@ -325,8 +339,43 @@ def check_on_clicked(label):
 # Plotting all the curves
 ax.set_xlabel(r'$R$ (kpc)', fontsize=20)
 ax.set_ylabel(r'$v_c$ (km/s)', fontsize=20)
-ax.set_xlim([0, r_califa[-1]])
-ax.set_ylim([0,300])
+ax.set_xlim([0, r_data[-1]])
+ax.set_ylim([0,np.max(v_c_data)*1.1])
 check.on_clicked(check_on_clicked)
-plt.ylim([0,300])
+plt.ylim([0,np.max(v_c_data)*1.1])
 plt.show()
+
+chk=[]
+if MN_b_plot.get_visible() == True:
+    chk.append(True)
+else:
+    chk.append(False)
+if MN_td_plot.get_visible() == True:
+    chk.append(True)
+else:
+    chk.append(False)
+if MN_tkd_plot.get_visible() == True:
+    chk.append(True)
+else:
+    chk.append(False)
+if EX_d_plot.get_visible() == True:
+    chk.append(True)
+else:
+    chk.append(False)
+if NFW_plot.get_visible() == True:
+    chk.append(True)
+    print "La Masa virial del modelo NFW es: ", '%E' % NFW_p.mvir()
+    print "El parámetro de concentracion del modelo NFW es: ",'%E' % NFW_p.conc()
+else:
+    chk.append(False)
+
+compnts = ['BULGE','THIN DISK','THICK DISK','EXP DISK', 'DARK HALO']
+masses = [amp1, amp2, amp3, amp4, amp5]
+aa = [a1, a2, a3, h_r, a5]
+bb = [b1, b2, b3, 0, 0]
+#chk = [True,True,False,False,False]
+
+
+output_parameters = Table.Table([compnts,masses, aa,bb, chk], names=('component', 'mass', 'a (kpc)', 'b (kpc)', 'checked'), meta={'name': 'output_parameters'})
+
+output_parameters.write('output_parameters.txt', format='ascii.tab', overwrite=True)